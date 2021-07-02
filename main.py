35dabdb6d0a2edcb0451726fcddee1e9613f9ced
Dorian Latouche <dorian.latouche@hotmail.com>
2021-07-02 15:59:12 +0200

    Add keyboard event for move player

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-from player import Player
+from assets.constants import Direction
+from game import Game
 import pygame
 pygame.init()
 
 # Create game window
-pygame.display.set_caption("Bomberman", "/assets/bomber.png")
+logo = pygame.image.load("assets/logo.png")
+pygame.display.set_icon(logo)
+pygame.display.set_caption("Bomberman")
 screen = pygame.display.set_mode((1000, 1000))
+
+# Load game
+game = Game(screen)
 
 # Run the window
 running = True
 while running:
+
+    # Clear screen
+    screen.fill((0, 0, 0))
+
+    # Load player
+    screen.blit(game.player.image, game.player.rect)
+
+    # check key used
+    # top deplacement
+    if game.pressed.get(pygame.K_z):
+        game.player.move(Direction.TOP)
+    # right deplacement
+    elif game.pressed.get(pygame.K_d):
+        game.player.move(Direction.RIGHT)
+    # bottom deplacement
+    elif game.pressed.get(pygame.K_s):
+        game.player.move(Direction.BOTTOM)
+    # left deplacement
+    elif game.pressed.get(pygame.K_q):
+        game.player.move(Direction.LEFT)
+
+    # Update screen
+    pygame.display.flip()
 
     # Check window event
     for event in pygame.event.get():
@@ -16,3 +45,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
+        # Check keybord event
+        elif event.type == pygame.KEYDOWN:
+            game.pressed[event.key] = True
+        elif event.type == pygame.KEYUP:
+            game.pressed[event.key] = False
---
+++ player.py
@@ -0,0 +1,26 @@
+from assets.constants import Direction
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.velocity = 1
+        self.nb_bomb = 1
+        self.life = 1
+        self.image = pygame.image.load('./assets/player.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = 200
+        self.rect.y = 200
+
+    def move(self, direction):
+        if direction == Direction.TOP and self.rect.y > 0:
+            self.rect.y -= self.velocity
+        elif direction == Direction.RIGHT and self.rect.x + self.rect.width < self.game.screen.get_width():
+            self.rect.x += self.velocity
+        elif direction == Direction.BOTTOM and self.rect.y + self.rect.height < self.game.screen.get_height():
+            self.rect.y += self.velocity
+        elif direction == Direction.LEFT and self.rect.x > 0:
+            self.rect.x -= self.velocity
